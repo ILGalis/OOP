1431df93b75d4c6e84cef9384c354ca30779372f
ILGalis <vorligin2@gmail.com>
2026-09-07 12:00:06 +0700

    фикс чекстайла

--- Task_1_1_1/src/main/java/HeapSort.java
+++ Task_1_1_1/src/main/java/HeapSort.java
@@ -1,42 +1,75 @@
 package ru.example;
-
+/**
+ * Реализация пирамидальной сортировки.
+ * Сортирует массив на месте за O(n log n) времени, память O(1) соответственно.
+ */
 public class HeapSort {
-    public static void heapsift(int[] arr, int n, int i){
-        while (true){
-            int left = 2*i+1;
-            int right = 2*i+2;
+
+    /**
+     * Просеивает элемент вниз по куче.
+     *
+     * @param arr массив
+     * @param n   размер кучи
+     * @param i   индекс элемента для просеивания
+     */
+    public static void heapsift(int[] arr, int n, int i) {
+        while (true) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
             int largest = i;
-            if (left < n && arr[left] > arr[largest]){
+            if (left < n && arr[left] > arr[largest]) {
                 largest = left;
             }
-            if (right < n && arr[right] > arr[largest]){
+            if (right < n && arr[right] > arr[largest]) {
                 largest = right;
             }
             if (largest == i) {
                 break;
             }
-            swap(arr,i,largest);
-            i=largest;
+            swap(arr, i, largest);
+            i = largest;
         }
     }
-    public static void swap(int[] arr, int i, int n){
+
+    /**
+     * Меняет местами два элемента в массиве.
+     *
+     * @param arr массив
+     * @param j   индекс элемента первого
+     * @param i   индекс элемента второго
+     */
+    public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
-        arr[i]=arr[n];
-        arr[n]=temp;
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
-    public static void build(int[] arr){
+
+    /**
+     * Строит кучу, используя просеивание.
+     *
+     * @param arr массив
+     */
+    public static void build(int[] arr) {
         int len = arr.length;
-        for (int i=len/2 - 1;i>=0;i--){
-            heapsift(arr,len,i);
+        for (int i = len / 2 - 1; i >= 0; i--) {
+            heapsift(arr, len, i);
         }
     }
-    public static int[] sort(int[] arr){
+
+    /**
+     * Сортирует уже сам массив пирамидальной сортировкой.
+     *
+     * @param arr массив
+     * @return отсортированный массив
+     */
+    public static int[] sort(int[] arr) {
         int len = arr.length;
         build(arr);
-        for (int i=len-1;i>=0;i--){
-            swap(arr,0,i);
-            heapsift(arr,i,0);
+        for (int i = len - 1; i >= 0; i--) {
+            swap(arr, 0, i);
+            heapsift(arr, i, 0);
         }
         return arr;
     }
+
     }
