eea6e0f2d54c75562e999a7f9e53373531fbff41
ILGalis <ILGalis@users.noreply.github.com>
2026-09-07 04:34:56 +0000

    Deploying to gh-pages from @ ILGalis/OOP@e8a60536fdbae979012227a0260edb654d742795 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.example","l":"Main"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.example","l":"HeapSort"}];updateSearchResults();
